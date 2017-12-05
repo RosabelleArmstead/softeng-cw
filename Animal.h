8fc30c0e57827b4725328d386c44271538099d25
Nick Frymann <nick.frymann@gmail.com>
2017-12-05 04:11:12 +0000

    Removed Animal setters and added parent accessors

--- Animal.h
+++ Animal.h
@@ -25,59 +25,40 @@ public:
   // input -- None
   // output -- None
 
-  void setName(string name);
-  // purpose -- to set the name of an Animal
-  // input -- the name of the Animal
-  // output -- None
   string getName();
   // purpose -- to get the name of an Animal
   // input -- None
   // output -- the name of the Animal
 
-  void setBreed(string breed);
-  // purpose -- to set the breed of an Animal
-  // input -- the breed of the Animal
-  // output -- None
   string getBreed();
   // purpose -- to get the breed of an Animal
   // input -- None
   // output -- the breed of the Animal
 
-  void setColour(string colour);
-  // purpose -- to set the colour of an Animal
-  // input -- the colour of the Animal
-  // output -- None
   string getColour();
   // purpose -- to get the colour of an Animal
   // input -- None
   // output -- the colour of the Animal
 
-  void setEarType(string earType);
-  // purpose -- to set the Ear Type of an Animal
-  // input -- the Ear Type of the Animal
-  // output -- None
   string getEarType();
   // purpose -- to get the Ear Type of an Animal
   // input -- None
   // output -- the Ear Type of the Animal
 
-  void setHeight(double height);
-  // purpose -- to set the height of an Animal
-  // input -- the height of the Animal
-  // output -- None
   int getHeight();
   // purpose -- to get the height of an Animal
   // input -- None
   // output -- the height of the Animal
 
-  void setTailColour(string tailColour);
-  // purpose -- to set the tail colour of an Animal
-  // input -- the tail colour of the Animal
-  // output -- None
   string getTailColour();
   // purpose -- to get the tail colour of an Animal
   // input -- None
   // output -- the tail colour of the Animal
+
+  virtual void setFather(const Animal param_father);
+  virtual const Animal getFather();
+  virtual Animal setMother(const Animal param_mother);
+  virtual const Animal getMother();
 };
 
 #endif
