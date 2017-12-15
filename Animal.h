966224165acaed93c5eb832cdb2ccb5ecb15de4c
sam_rowe <sr00584@surrey.ac.uk>
2017-12-15 19:11:59 +0000

    Changed comments in animal.h

--- Animal.h
+++ Animal.h
@@ -8,6 +8,7 @@ using namespace std;
 class Animal {
 
   private:
+    // All animals must have these attributes
     string name;
     string breed;
     string colour;
@@ -16,61 +17,61 @@ class Animal {
     string tailColour;
 
   public:
-    Animal(string param_name, string param_breed, string param_colour,
-           string param_earType, string param_height, string param_tailColour);
     // purpose --  to create a new animal object
     // input --  the different attributes that an animal must have including name, breed, colour, eartype, height and tailcolour
     // output -- None
+    Animal(string param_name, string param_breed, string param_colour,
+           string param_earType, string param_height, string param_tailColour);
 
-    inline const string getName() const { return name; }
     // purpose -- to retrieve the name of an animal
     // input -- None
     // output --  the name of the animal
+    inline const string getName() const { return name; }
 
-    inline const string getBreed() const { return breed; }
     // purpose -- to retrieve the breed of an animal
     // input -- None
     // output --  the breed of the animal
+    inline const string getBreed() const { return breed; }
 
-    inline const string getColour() const { return colour; }
     // purpose -- to retrieve the colour of an animal
     // input -- None
     // output --  the colour of the animal
+    inline const string getColour() const { return colour; }
 
-    inline const string getEarType() const { return earType; }
     // purpose -- to retrieve the ear type of an animal
     // input -- None
     // output --  the ear type of the animal
+    inline const string getEarType() const { return earType; }
 
-    inline const string getHeight() const { return height; }
     // purpose -- to retrieve the height of an animal
     // input -- None
     // output --  the height of the animal
+    inline const string getHeight() const { return height; }
 
-    inline const string getTailColour() const { return tailColour; }
     // purpose -- to retrieve the tail colour of an animal
     // input -- None
     // output --  the tail colour of the animal
+    inline const string getTailColour() const { return tailColour; }
 
-    virtual const Animal* getFather() const = 0;
     // purpose -- to retrieve the father of a specific animal
     // input -- None
     // output --  the father of an animal which is of the specific type of the animal
+    virtual const Animal* getFather() const = 0;
 
-    virtual const Animal* getMother() const = 0;
     // purpose -- to retrieve the mother of a specific animal
     // input -- None
     // output --  the mother of an animal which is of the specific type of the animal
+    virtual const Animal* getMother() const = 0;
 
-    virtual const string getAnimalType() const = 0;
     // purpose -- to find the specific type of animal e.g. Cat, Dog or Horse
     // input -- None
     // output --  the type of the animal that the method has been called on e.g. Cat, Dog or Horse
+    virtual const string getAnimalType() const = 0;
 
-    void printPaternalTree() const;
     // purpose -- to retrieve the paternal tree of a specific animal
     // input -- None
     // output --  the paternal tree of the animal given
+    void printPaternalTree() const;
 
 };
 
